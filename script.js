0ba09109a2a2ff94b139b735a9c5f8aad4e8e1b4
BlayzeRod <gregoryp161@gmail.com>
2024-03-15 10:06:01 +0100

    Ajout de la tracklist et du lecteur de musique

--- script.js
+++ script.js
@@ -7,15 +7,14 @@ fetch('dataunform.json')
 
 window.addEventListener('load', () =>{
     d3.json('dataunform.json').then((data)=> {
-    for (var i=0; i < data.length; i++){
-        console.log(data[i].album.name);
         setTrackList(data)
-    }
-});
+    });
 });
 
 function setTrackList(data){
     let template = document.getElementById('trackCard');
+    let trackListContainer = document.getElementById('trackList');
+    trackListContainer.innerHTML = '';
 
     for (let i = 0; i < data.length; i++) {
         const clone = template.content.cloneNode(true);
@@ -24,7 +23,15 @@ function setTrackList(data){
         clone.querySelector('.card-title').textContent = data[i].name;
         clone.querySelector('.card-img-top').src = data[i].album.images[0].url;
         clone.querySelector('.card-img-top').alt = data[i].name;
-
-        document.getElementById('trackList').appendChild(clone);
+        clone.querySelector('.card').addEventListener('click', () => {
+          playTrack(data[i].preview_url);
+      });
+      trackListContainer.appendChild(clone);
     }
+}
+
+function playTrack(previewUrl) {
+  let audioPlayer = document.getElementById('audioPlayer');
+  audioPlayer.src = previewUrl;
+  audioPlayer.play();
 }
